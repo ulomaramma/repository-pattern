052d7d82c9e56633f33b168d8d7244a48ce37dcf
Uloma Okenyi <cyndibilo@gmail.com>
2023-02-06 10:14:46 +0000

    Resolved Bug in Application DB COntext

--- RepositoryPattern/Domain/Entities/Food.cs
+++ RepositoryPattern/Domain/Entities/Food.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoodRecipe.DataAccess.Data.Entities
+namespace Domain.Entities
 {
-    public class ApiAuditInfo
+    internal class Food
     {
-
     }
 }
