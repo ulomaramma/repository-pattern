949f86507051ca62366d23b003f4d3a65889c329
Uloma Okenyi <cyndibilo@gmail.com>
2023-02-22 14:28:47 +0000

    modified program.cs

--- RepositoryPattern/WebApi/Program.cs
+++ RepositoryPattern/WebApi/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddTransient<IUnitOfWork,UnitOfWork>();
 builder.Services.AddScoped<RecipeService>();
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
